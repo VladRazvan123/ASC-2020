bd0a448f218373c72a10a60c9aa7c50feeb4d8f5
Vlad Razvan <lewis34lewis@yahoo.com>
2020-10-09 17:38:15 +0300

    creare proiect Hello World

---
+++ ASCR1/ASCR1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASCR1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World of C#!!");
+        }
+    }
+}
